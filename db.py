3738224a3501d75cdfe6065655bc964173ad3f1d
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2025-02-01 23:25:21 +0700

    Update

--- db.py
+++ db.py
@@ -1,20 +1,86 @@
 import mysql.connector
+import pandas as pd
 
-
-mydb = mysql.connector.connect(
-    host="autorack.proxy.rlwy.net",
+dbConn = mysql.connector.connect(
+    host="roundhouse.proxy.rlwy.net",
     user="root",
-    password="SZbaEeOIWDmDAwbijBuTEIhGeYrymmUP",
-    port="26815",
+    password="gKMzXAztwBBbgpIclBMYSXiwTnBrtBrq",
+    port="20726",
     database="railway",
 )
 
-mycursor = mydb.cursor()
 
-mycursor.execute("""CREATE TABLE Persons (
-    PersonID int,
-    LastName varchar(255),
-    FirstName varchar(255),
-    Address varchar(255),
-    City varchar(255)
-);""")
+#################################
+# Create table
+#################################
+# cursor = dbConn.cursor()
+# cursor.execute("""DROP TABLE IF EXISTS Donation;""")
+# cursor.execute("""CREATE TABLE Donation (
+#     id int NOT NULL AUTO_INCREMENT,
+#     name varchar(255),
+#     amount int NOT NULL,
+#     comment varchar(255) NOT NULL DEFAULT '',
+#     createdAt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+#     updatedAt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+#     PRIMARY KEY (id)
+# );""")
+# dbConn.commit()
+# cursor.close()
+# dbConn.close()
+
+#################################
+# Insert data
+#################################
+# cursor = dbConn.cursor()
+# cursor.execute(
+#     """INSERT INTO Donation (name, amount, comment) VALUES ('Tom', 100, 'OK');"""
+# )
+# dbConn.commit()
+# cursor.close()
+# dbConn.close()
+
+#################################
+# Read data
+#################################
+# cursor = dbConn.cursor()
+# cursor.execute("""SELECT * FROM Donation;""")
+# results = cursor.fetchall()
+# df = pd.DataFrame(
+#     results, columns=["id", "name", "amount", "comment", "createdAt", "updatedAt"]
+# )
+# df["createdAt"] = df["createdAt"].dt.strftime("%Y-%m-%d %H:%M:%S")
+# df["updatedAt"] = df["updatedAt"].dt.strftime("%Y-%m-%d %H:%M:%S")
+# donations = df.to_dict(orient="records")
+# print(donations)
+# cursor.close()
+# dbConn.close()
+
+
+#################################
+# Modify data
+#################################
+# cursor = dbConn.cursor()
+# cursor.execute("""UPDATE Donation SET name = 'John' WHERE id = 1;""")
+# dbConn.commit()
+# cursor.close()
+# dbConn.close()
+
+
+#################################
+# Delete data (by id)
+#################################
+# cursor = dbConn.cursor()
+# cursor.execute("""DELETE FROM Donation WHERE id = 1;""")
+# dbConn.commit()
+# cursor.close()
+# dbConn.close()
+
+
+#################################
+# Delete all data
+#################################
+# cursor = dbConn.cursor()
+# cursor.execute("""DELETE FROM Donation;""")
+# dbConn.commit()
+# cursor.close()
+# dbConn.close()
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from typing import Annotated
 
-from fastapi import FastAPI, Request, Form
+from fastapi import FastAPI, Form, Request
 from fastapi.templating import Jinja2Templates
+from db import dbConn
+import pandas as pd
 
 templates = Jinja2Templates(directory="templates")
 
@@ -11,15 +13,45 @@ app = FastAPI()
 
 @app.get("/")
 def homepage_get(request: Request):
-    name = "Hello World"
+    donations = getAllDonations()
     return templates.TemplateResponse(
-        request=request, name="page.html", context={"name": name}
+        request=request, name="page.html", context={"donations": donations}
     )
 
 
 @app.post("/")
-def homepage_post(name: Annotated[str, Form()], request: Request):
-    print(name)
+def homepage_post(
+    name: Annotated[str, Form()],
+    amount: Annotated[int, Form()],
+    comment: Annotated[str, Form()],
+    request: Request,
+):
+    insertDonation(name, amount, comment)
+    donations = getAllDonations()
     return templates.TemplateResponse(
-        request=request, name="page.html", context={"name": name}
+        request=request, name="page.html", context={"donations": donations}
     )
+
+
+def insertDonation(name, amount, comment):
+    cursor = dbConn.cursor()
+    cursor.execute(
+        "INSERT INTO Donation (name, amount, comment) VALUES (%s, %s, %s)",
+        (name, amount, comment),
+    )
+    dbConn.commit()
+    cursor.close()
+
+
+def getAllDonations():
+    cursor = dbConn.cursor()
+    cursor.execute("SELECT * FROM Donation")
+    results = cursor.fetchall()
+    cursor.close()
+    df = pd.DataFrame(
+        results, columns=["id", "name", "amount", "comment", "createdAt", "updatedAt"]
+    )
+    df.sort_values(by="createdAt", ascending=False, inplace=True)
+    df["createdAt"] = df["createdAt"].dt.strftime("%Y-%m-%d %H:%M:%S")
+    df["updatedAt"] = df["updatedAt"].dt.strftime("%Y-%m-%d %H:%M:%S")
+    return df.to_dict(orient="records")
