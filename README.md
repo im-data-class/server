0931296038e1aedd7c11cae8dbcee99792425ef4
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2025-02-01 13:49:47 +0700

    Update

---
+++ README.md
@@ -0,0 +1,3 @@
+# Server
+
+- `fastapi dev main.py`
--- main.py
+++ main.py
@@ -1,16 +1,25 @@
-from typing import Union
+from typing import Annotated
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Form
+from fastapi.templating import Jinja2Templates
+
+templates = Jinja2Templates(directory="templates")
 
 
 app = FastAPI()
 
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def homepage_get(request: Request):
+    name = "Hello World"
+    return templates.TemplateResponse(
+        request=request, name="page.html", context={"name": name}
+    )
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/")
+def homepage_post(name: Annotated[str, Form()], request: Request):
+    print(name)
+    return templates.TemplateResponse(
+        request=request, name="page.html", context={"name": name}
+    )
